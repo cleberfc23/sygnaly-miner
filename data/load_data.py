2482a8b80ec68fa2a04f2bf347d5564a97ac0dc5
Cleber F. Carvalho <cleberfc23@gmail.com>
2026-03-22 21:19:51 -0300

    add streamlit on load_data

--- data/load_data.py
+++ data/load_data.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from pathlib import Path
+import streamlit as st
+
+
 # BASE_DIR = Path(__file__).resolve().parent
 # DATA_PATH = BASE_DIR / "dev" / "sygnaly_dev_clean_3000.csv"
 
@@ -11,7 +14,9 @@ from pathlib import Path
 
 
 
+
+@st.cache_data
 def load_data():
-    file_id = '1F_ADRBo3cEBLdF4fHsv7mGCYZXjWad7l'
-    path_url = f'https://drive.google.com/uc?id={file_id}'
-    return pd.read_csv(path_url)
+    file_id = "1F_ADRBo3cEBLdF4fHsv7mGCYZXjWad7l"
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    return pd.read_csv(url)
