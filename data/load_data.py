9557740bbd28743682795504d9464ddec0ce32e8
Cleber F. Carvalho <cleberfc23@gmail.com>
2026-03-22 21:16:40 -0300

    update requirements

--- data/load_data.py
+++ data/load_data.py
@@ -2,8 +2,16 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent
-DATA_PATH = BASE_DIR / "dev" / "sygnaly_dev_clean_3000.csv"
+# BASE_DIR = Path(__file__).resolve().parent
+# DATA_PATH = BASE_DIR / "dev" / "sygnaly_dev_clean_3000.csv"
+
+# def load_data():
+#     path_url = 'https://drive.google.com/file/d/1F_ADRBo3cEBLdF4fHsv7mGCYZXjWad7l/view?usp=sharing'
+#     return pd.read_csv(path_url)
+
+
 
 def load_data():
-    return pd.read_csv(DATA_PATH)
+    file_id = '1F_ADRBo3cEBLdF4fHsv7mGCYZXjWad7l'
+    path_url = f'https://drive.google.com/uc?id={file_id}'
+    return pd.read_csv(path_url)
